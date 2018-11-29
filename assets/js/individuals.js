b561e1d315357505137d3cd15e74fe0fab3bf936
Tony Wu <ttwu@ucsd.edu>
2018-11-29 11:29:05 -0800

    adding functionality to toggles

--- assets/js/individuals.js
+++ assets/js/individuals.js
@@ -1,1 +1,11 @@
 setActivePage('nav_indiv');
+
+$('.ui.toggle').checkbox();
+
+$('.ui.toggle').click(function() {
+  if ($('.ui.toggle').checkbox('is checked')) {
+    console.log('Toggle On');
+  } else {
+    console.log('Toggle Off');
+  }
+});
